651422362c2a37fa9cef0735553a9eb34979eb1b
tehlordvortex <tehlordvortex@outlook.com>
2018-05-06 08:07:42 +0100

    remove unused dependencies

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -8,13 +8,11 @@ import './animate.css'
 import '../../static/material-icons/material-icons.css'
 import Spinner from 'vue-spinkit'
 import SweetModal from 'sweet-modal-vue/src/plugin.js'
-import VTooltip from 'v-tooltip'
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 Vue.use(VueAsyncComputed)
 Vue.use(SweetModal)
-Vue.use(VTooltip)
 Vue.component('Spinner', Spinner)
 
 /* eslint-disable no-new */
