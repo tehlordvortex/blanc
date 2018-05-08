7897ca7af1bf94fa39fb26656b88d5a84c30478f
tehlordvortex <tehlordvortex@outlook.com>
2018-05-08 03:01:24 +0100

    remove uneeded settings

--- src/renderer/lib/settings.js
+++ src/renderer/lib/settings.js
@@ -1,31 +1,10 @@
 import settingsStore from 'electron-settings'
 
 export const defaults = {
-  volume: 1,
-  muted: false,
-  playbackRate: 1,
   libraries: []
 }
 
 class Settings {
-  get volume () {
-    return settingsStore.get('volume', defaults.volume)
-  }
-  set volume (v) {
-    settingsStore.set('volume', v)
-  }
-  get muted () {
-    return settingsStore.get('muted', defaults.muted)
-  }
-  set muted (m) {
-    settingsStore.set('muted', m)
-  }
-  get playbackRate () {
-    return settingsStore.get('playbackRate', defaults.playbackRate)
-  }
-  set playbackRate (pbr) {
-    settingsStore.set('playbackRate', pbr)
-  }
   get libraries () {
     return settingsStore.get('libraries', defaults.libraries)
   }
