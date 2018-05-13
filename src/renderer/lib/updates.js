86b666701fcb816d1e9aed53466e5b7e569a75d7
tehlordvortex <tehlordvortex@outlook.com>
2018-05-13 15:45:08 +0100

    update update logic to handle version changes

--- src/renderer/lib/updates.js
+++ src/renderer/lib/updates.js
@@ -1,16 +1,28 @@
 import settings from './settings'
-// import { albumsDB, colorsDB, default as db } from '@/library.db'
-import { indexAlbums } from '@/lazy-loaders'
+import { colorsDB, default as db } from '@/library.db'
+import { join } from 'path'
+import { indexAlbums, artsCachePath } from '../lazy-loaders'
 
 export default function finishUpdate () {
-  return new Promise((resolve, reject) => {
-    if (settings.lastRunVersion < '0.5.0') {
-      indexAlbums()
-        .then(() => {
-          resolve()
-        })
-    } else {
-      resolve()
-    }
-  })
+  let p = Promise.resolve()
+  if (settings.lastRunVersion < '0.5.0') {
+    console.log('Re-indexing albums')
+    p = p.then(() => indexAlbums())
+  }
+  if (settings.lastRunVersion < '0.7.1') {
+    console.log('Re-caching colors')
+    p = p.then(() => {
+      return colorsDB.find({}).then(docs => {
+        let resolves = Promise.resolve()
+        for (const colorDoc of docs) {
+          let albumArtPath = join(artsCachePath, colorDoc._id)
+          resolves = resolves.then(() => {
+            return db.update({ albumArt: albumArtPath }, { $set: { colors: colorDoc.colors } }, { multi: true })
+          })
+        }
+        return resolves
+      })
+    })
+  }
+  return p
 }
